4b51a4ddf3f71fd1c9f2fb23b95af8400170f4f1
wonkatin <j.drawers@gmail.com>
2021-04-05 19:03:48 -0500

    class work

--- lab.js
+++ lab.js
@@ -7,8 +7,43 @@ console.clear();
 // console.log(realNumber) => 32
 var numberString = '42';
 var realNumber = 7;
+var newVariable = realNumber.toString();
+realNumber = parseInt(numberString);
+numberString = newVariable;
+console.log(typeof (numberString), numberString);
+console.log(typeof (realNumber), realNumber);
+// todo: swap the values in the variables and log the results
+/* --- Problem #2 - function and mixed array type --- */
+// 1. create an array type that is a mix of strings of numbers and actual numbers
+// ex: [10, '34', 3, '17']
+// 2. write a function that accepts an array as a parameter, loops over it and converts all the 
+// strings to numbers. the function should return a new array of only numbers
+// ex: stringToNum([10, '34', 3, '17']) => [10, 34, 3, 17]
+// Todo write the mixed array of strings and numbers
+var mixedArray = ['29', 29, 10, '10', 2, '2'];
+// Todo write a function to convert the mixed array to an array of only numbers
+var newArray = [];
+function converter(array) {
+    array.forEach(function (item) {
+        if (typeof (item) === "string") {
+            var newItem = parseInt(item);
+            newArray.push(newItem);
+        }
+        else {
+            newArray.push(item);
+        }
+    });
+    return newArray;
+}
+console.log(converter(mixedArray));
+console.log(newArray);
+var chicago = ['chicago', 20, 30];
+var portland = ['portland', 30, 40];
+// 🚨 uncomment this function to test your solution 🚨
+function printCityData(city) {
+    console.log(city[0] + " is at Lat: " + city[1] + ", Long: " + city[2]);
+}
+printCityData(chicago);
+printCityData(portland);
 // Todo - Create union type here
 // Todo - write area function here
-/* --- Problem #5 - Classes --- */
-// Bonus - Implement a Stack with TypeScript
-// https://initjs.org/data-structure-stack-in-javascript-714f45dbf889
--- lab.ts
+++ lab.ts
@@ -9,8 +9,15 @@ console.clear()
 
 let numberString: string = '42'
 let realNumber: number = 7
+let newVariable: string = realNumber.toString()
+realNumber = parseInt(numberString)
+numberString = newVariable
+console.log(typeof(numberString), numberString)
+console.log(typeof(realNumber), realNumber)
 
-// todo: swap the values in the varaibles and log the results
+
+
+// todo: swap the values in the variables and log the results
 
 /* --- Problem #2 - function and mixed array type --- */
 // 1. create an array type that is a mix of strings of numbers and actual numbers
@@ -20,20 +27,45 @@ let realNumber: number = 7
 // ex: stringToNum([10, '34', 3, '17']) => [10, 34, 3, 17]
 
 // Todo write the mixed array of strings and numbers
-
+let mixedArray: (string | number)[] = ['29', 29, 10, '10', 2, '2']
 // Todo write a function to convert the mixed array to an array of only numbers
 
+let newArray: number[] = []
+
+function converter(array: (string | number)[]): number[] {
+    array.forEach(item => {
+        if(typeof(item) === "string") {
+            let newItem = parseInt(item)
+            newArray.push(newItem)
+        }
+        else {
+            newArray.push(item)
+        }
+    })
+    return newArray
+}
+console.log(converter(mixedArray))
+
+console.log(newArray)
+
+
 /* --- Problem #3 - Tuple Type --- */
-// 1. Create a tuple type named 'cityCoordinate' that has three elements: city name, latitute, and longitude
+// 1. Create a tuple type named 'cityCoordinate' that has three elements: city name, latitude, and longitude
 // 2. Create some cities with lat/lon data and pass them into printCityData;
 // uncomment the function to test your solution
 
 // Todo - create the type definition, and some cities
+type cityCoordinate = [string, number, number]
+
+let chicago: cityCoordinate = ['chicago', 20, 30]
+let portland: cityCoordinate = ['portland', 30, 40]
 
 // 🚨 uncomment this function to test your solution 🚨
-// function printCityData(city: cityCoordinate): void {
-//     console.log(`${city[0]} is at Lat: ${city[1]}, Long: ${city[2]}`);
-// }
+function printCityData(city: cityCoordinate): void {
+    console.log(`${city[0]} is at Lat: ${city[1]}, Long: ${city[2]}`);
+}
+printCityData(chicago)
+printCityData(portland)
 
 // printCityData(seattle);
 
@@ -42,8 +74,8 @@ let realNumber: number = 7
 // 1. Create a union type that is the union of the Square, Rectangle, and Circle interfaces
 // 2. Write the area function that: 1. Only takes in your union type as an input 2. Returns the area of the shape
 // oh no! math!
-// the area of a square is found by multipling the width by the width
-// the area of a rectangle is found by multipling the width by the height
+// the area of a square is found by multiplying the width by the width
+// the area of a rectangle is found by multiplying the width by the height
 // the area of a circle is found by multiplying pi by the radius squared
 
 interface Square {
